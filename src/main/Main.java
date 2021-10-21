b35f929edc161c9684e857e83a0debc5963cda5b
TechnicFive <flufi@usal.es>
2021-10-21 13:15:16 +0200

    Inicia el log4j

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 
 import operaciones.OpBasicas;
+
+import java.net.URL;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
 import operaciones.Comprar;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 
 		OpBasicas inicio = new OpBasicas();
 
@@ -18,14 +25,20 @@ public class Main {
 		comprar.comprarAleatorio(OpBasicas.vagon);
 
 		comprar.comprarAleatorio(OpBasicas.vagon);
+		
+		
+		
 
 		inicio.limpiar();
 
 		comprar.comprarAleatorio(OpBasicas.vagon);
 
 		comprar.comprarAleatorio(OpBasicas.vagon);
+		
+		
 
 		comprar.comprarNominal(OpBasicas.vagon);
+		
 
 		comprar.comprarAleatorio(OpBasicas.vagon);
 
@@ -34,7 +47,10 @@ public class Main {
 		comprar.comprarAleatorio(OpBasicas.vagon);
 
 		comprar.comprarAleatorio(OpBasicas.vagon);
+
 		
 	}
+	private static Logger logger = LogManager.getLogger(Main.class);
 
 }
+
